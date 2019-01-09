71f48ea772e3f52f4fec51fcc75b05a74ea54a10
Shlomi.O <shlomio@codevalue.net>
2019-01-09 11:35:54 +0200

    +WebApplication3 ++HelloController."api/HelloJson" JsonResult.

---
+++ WebApplication3/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication3.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet(@"api/HelloJson")]
+        public JsonResult GetHelloJson()
+        {
+            return new JsonResult(new List<object>
+                {
+                    "HelloJson"
+                }
+            );
+        }
+    }
+}
