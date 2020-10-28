f89e3940fa8d6ff82109aef290863c1d8b0617d3
ShloEmi <ShloEmi>
2020-10-28 21:36:27 +0200

    +RngController

---
+++ MyCraft/Controllers/RngController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyCraft.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RngController : ControllerBase
+    {
+        private readonly ILogger<RngController> logger;
+        private readonly Random rng = new Random();
+
+        public RngController(ILogger<RngController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            logger.LogTrace($"{nameof(RngController)} called");
+            return rng.Next(1, 6);
+        }
+    }
+}
